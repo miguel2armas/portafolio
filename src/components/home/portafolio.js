7a54922e3b1f9d660b0065f84d7024a9f760e41d
migue <miguel.armas.developer@gmail.com>
2021-09-08 00:34:33 -0500

    portafolio

--- src/components/home/portafolio.js
+++ src/components/home/portafolio.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {Carousel, Col, Row} from "react-bootstrap";
 import img1 from "../../assets/img/img_page/e-tournament.webp"
+import FondTransp from "../../assets/img/img_page/fond_transp.png"
+import {Link} from "react-router-dom";
 
 class Portafolio extends Component {
     render() {
@@ -8,31 +10,53 @@ class Portafolio extends Component {
             <div>
 
                 <Carousel>
-                    <Carousel.Item >
-                        <img src={img1}/>
+                    <Carousel.Item interval={1500}>
+                        <img src={FondTransp}/>
                         <Carousel.Caption>
                             <Row>
                                 <Col>
-                                    <img src={img1} className="img-fluid"/>
+                                    <img src={img1} className="rounded img-fluid"/>
+                                </Col>
+                                <Col>
+                                    <div className="text-start">
+                                        <h3 className={this.props.checkedTheme?('text-white'):('text-dark')}>E-tournament</h3>
+                                        <h4 className={this.props.checkedTheme?('text-white'):('text-dark')}>Pagina de torneos de juegos electronicos
+                                            con creador de torneos y venta por medio de la pasarela de pago de mercado libre en proceso</h4>
+                                        <Link className="btn btn-theme my-5">Visitar</Link>
+                                    </div>
+                                </Col>
+                            </Row>
+                    </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item interval={1500}>
+                        <img src={FondTransp}/>
+                        <Carousel.Caption>
+                            <Row>
+                                <Col>
+                                    <img src={img1} className="rounded img-fluid"/>
                                 </Col>
                                 <Col>
                                     text
                                 </Col>
                             </Row>
-                    </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item interval={500}>
-                        <img src={img1}/>
-                        <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                         </Carousel.Caption>
                     </Carousel.Item>
-                    <Carousel.Item>
-                        <img src={img1}/>
+                    <Carousel.Item interval={1500}>
+                        <img src={FondTransp}/>
                         <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            <Row>
+                                <Col>
+                                    <img src={img1} className="rounded img-fluid"/>
+                                </Col>
+                                <Col>
+                                    <div>
+                                        <h3>E-tournament</h3>
+                                        <h4>Pagina de torneos de juegos electronicos
+                                            con creador de torneos y venta por medio de la pasarela de pago de mercado libre en proceso</h4>
+                                        <Link className="btn btn-theme">Visitar</Link>
+                                    </div>
+                                </Col>
+                            </Row>
                         </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
