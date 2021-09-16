7396706f5a89dacd942dc0228c86ab537aaf2ba9
migue <miguel.armas.developer@gmail.com>
2021-09-16 01:28:37 -0500

    ocultar configuracion

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,7 @@ import 'firebase/compat/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAtZC4j8ZnGRRh-RNJhdb-9LDMZL1UcbAI",
-    authDomain: "portafolio-miguel-ec613.firebaseapp.com",
-    projectId: "portafolio-miguel-ec613",
-    storageBucket: "portafolio-miguel-ec613.appspot.com",
-    messagingSenderId: "784016253542",
-    appId: "1:784016253542:web:37e2f5462ae544a98303a7",
-    measurementId: "G-DFF9QKG50L"
+    //code config is here
 };
 
 // Initialize Firebase
